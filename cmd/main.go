2976c47d27f87ccb223bb8f95ac289e939d37637
el_kokosik <moroz1486@gmail.com>
2025-05-17 22:54:37 +0300

    bootstrap project structure

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"GoShort/configs"
+	"GoShort/internal/auth"
+	"log"
+	"net/http"
+	"time"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	conf := configs.LoadConfig()
+	router := http.NewServeMux()
+	auth.NewAuthHandler(router, auth.AuthConfig{conf})
+
+	server := &http.Server{
+		Addr:         ":8080",
+		Handler:      router,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 5 * time.Second,
+		IdleTimeout:  120 * time.Second,
+	}
+
+	log.Println("server is running on port:", server.Addr)
+	log.Fatal(server.ListenAndServe())
 }
---
+++ internal/auth/handler.go
@@ -0,0 +1,30 @@
+package auth
+
+import (
+	"GoShort/configs"
+	"log"
+	"net/http"
+)
+
+type AuthConfig struct {
+	*configs.Config
+}
+
+type AuthHandler struct {
+	*configs.Config
+}
+
+func NewAuthHandler(router *http.ServeMux, deps AuthConfig) {
+	handler := &AuthHandler{deps.Config}
+	router.HandleFunc("POST /auth/login", handler.Login)
+	router.HandleFunc("POST /auth/register", handler.Register)
+}
+
+func (handler *AuthHandler) Login(w http.ResponseWriter, r *http.Request) {
+	log.Println(handler.Config.Auth.Secret)
+	log.Println("Login")
+}
+
+func (handler *AuthHandler) Register(w http.ResponseWriter, r *http.Request) {
+	log.Println("Register")
+}
